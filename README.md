498fa554e7c3db4a0d61e6b545e34d7c34a220ce
guilhermefp1804 <guilherme.fpimentel@gmail.com>
2025-03-15 15:40:40 -0300

    projeto pronto, antes de revisão

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# Amigo secreto
+
+
+Esse projeto foi criado como resposta para o challenge do programa One da Oracle\Alura
+
+
+
+## Autores
+
+- [Guilherme Pimentel](https://www.github.com/guilhermefp1804)
+
--- app.js
+++ app.js
@@ -1,1 +1,53 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+
+function adicionarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+    amigo = amigo.trim();
+    console.log('aqui');
+    if (amigo != ''){
+        listaAmigos.push(amigo);
+        mostraNaTela('listaAmigos', montaListaAmigos());        
+    
+        
+        console.log(listaAmigos);
+        document.getElementById('amigo').value='';
+        document.getElementById('amigo').focus();
+
+    }
+    else{
+        alert('Por favor, insira um nome.');
+    }
+}
+
+function sortearAmigo(){
+    if(listaAmigos.length > 0){    
+        let indiceAmigo = parseInt((Math.random() * listaAmigos.length ));
+        let amigo = listaAmigos[indiceAmigo];
+        mostraNaTela('resultado', `O amigo sorteado foi: ${amigo}`);
+        listaAmigos.splice(indiceAmigo,1);
+        mostraNaTela('listaAmigos', montaListaAmigos());
+        console.log(listaAmigos, indiceAmigo);
+    }
+    else{
+        alert("não tem amigos na lista!")
+ 
+    }
+}
+
+function mostraNaTela(id, conteudo){
+    document.getElementById(id).innerHTML= conteudo;
+}
+
+function montaListaAmigos(){
+    let indice = 0;
+    let lista = '<li>Amigos adicionados</li>';
+
+    while (indice != listaAmigos.length ){
+        lista+=`<li>${listaAmigos[indice]}</li>`;        
+        indice++;
+    }
+
+    return lista;
+}
+    
