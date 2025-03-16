effa46ce9b5ecfbaebb8671f2af3a64460b9b3b1
guilhermefp1804 <guilherme.fpimentel@gmail.com>
2025-03-15 23:24:20 -0300

    versao final

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 # Amigo secreto
 
 
-Esse projeto foi criado como resposta para o challenge do programa One da Oracle\Alura
+Esse projeto foi criado como resposta para o challenge do programa One da Oracle\Alura.
 
 
 
@@ -10,3 +10,23 @@ Esse projeto foi criado como resposta para o challenge do programa One da Oracle
 
 - [Guilherme Pimentel](https://www.github.com/guilhermefp1804)
 
+# :hammer: Funcionalidades do projeto
+Para esse projeto foram criadas as seguintes funcões:
+
+
+ `adicionarAmigo()`: função ja referenciada no html, faz uma verificação se os dados 
+ adicionados não são vazios ou espaços, e adiciona o valor digitado a um array (listaAmigos).
+
+ `sortearAmigo()`: função ja referenciada no html, faz uma verificação se o array de amigos 
+ (listaAmigos) não está vazio, gera um numero randomico para sortear um amigo e mostra o resultado
+ na lista.
+
+ `adicionarAmigo()`: função ja referenciada no html, faz uma verificação se os dados 
+ adicionados não são vazios ou espaços, e adiciona o valor digitado a um array (listaAmigos).
+
+ `mostraNaTela(id, conteudo)`: recebe um id e um conteudo, e atualiza o elemento com o conteudo.
+
+ `montaListaAmigos()`: Cria uma lista de elementos "<li>" com os amigos no array (listaAmigos).
+ adicionamente gera um elemento a mais com a msg "Amigos adicionados" respeitando os plurais.
+
+ 
--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaAmigos = [];
 
 function adicionarAmigo() {
     let amigo = document.getElementById('amigo').value;
     amigo = amigo.trim();
-    console.log('aqui');
     if (amigo != ''){
         listaAmigos.push(amigo);
         mostraNaTela('listaAmigos', montaListaAmigos());        
-    
-        
-        console.log(listaAmigos);
         document.getElementById('amigo').value='';
         document.getElementById('amigo').focus();
-
     }
     else{
         alert('Por favor, insira um nome.');
@@ -21,18 +15,19 @@ function adicionarAmigo() {
 }
 
 function sortearAmigo(){
+    let resultadoSorteio ="";
     if(listaAmigos.length > 0){    
         let indiceAmigo = parseInt((Math.random() * listaAmigos.length ));
-        let amigo = listaAmigos[indiceAmigo];
-        mostraNaTela('resultado', `O amigo sorteado foi: ${amigo}`);
+        let amigo = listaAmigos[indiceAmigo];        
         listaAmigos.splice(indiceAmigo,1);
+        resultadoSorteio = `<li>O amigo sorteado foi: ${amigo}</li>`;
         mostraNaTela('listaAmigos', montaListaAmigos());
-        console.log(listaAmigos, indiceAmigo);
     }
     else{
-        alert("não tem amigos na lista!")
- 
+        alert("Não tem amigos na lista!")
     }
+    mostraNaTela('resultado', resultadoSorteio);
+
 }
 
 function mostraNaTela(id, conteudo){
@@ -41,13 +36,12 @@ function mostraNaTela(id, conteudo){
 
 function montaListaAmigos(){
     let indice = 0;
-    let lista = '<li>Amigos adicionados</li>';
+    let lista = listaAmigos.length > 1 ? '<li>Amigos adicionados</li>' : listaAmigos.length == 0 ? "" :'<li>Amigo adicionado</li>';
 
     while (indice != listaAmigos.length ){
         lista+=`<li>${listaAmigos[indice]}</li>`;        
         indice++;
     }
-
     return lista;
 }
     
